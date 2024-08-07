b1b6a9665877758e132d48df5e8787a88d2efd08
Alok Singh <aloksingh36757@gmail.com>
2024-08-07 23:11:33 +0530

    input statement

--- Program3.java
+++ Program3.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 class Program3{
+    public static void test(){
+        System.out.println("this is test method");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        String address = sc.nextLine();
+        char city = sc.nextLine().charAt(3);
+       
+        System.out.println("name is:-"+name);
+        System.out.println("my address is:-"+address);
+        System.out.println( "my city is "+city);
+
+    }
      public static void main(String[] args) {
         System.out.println("hello world");
         @SuppressWarnings("resource")
@@ -9,9 +21,10 @@ class Program3{
         int a=sc.nextInt();
         System.out.println("enter the second number:-");
         int b=sc.nextInt();
-     
+
         int sum = a+b;
-        System.out.println("the sum of two number"+sum);
+        System.out.println("The sum "+a+" and " +b+" is two number"+sum);
+        test();
         
     }
 }

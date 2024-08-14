a31ef587be84a6b3932dd1bab54e2af3edd59555
Alok Singh <aloksingh36757@gmail.com>
2024-08-14 20:34:59 +0530

    new files

---
+++ Program4.java
@@ -0,0 +1,17 @@
+import java.util.scanner
+
+class Big{
+public static void main(String[] args){
+Scanner sc = new Scanner(Sytem.in)
+System.out.println("Enter the first number:");
+int x =sc.nextInt();
+System.out.println("Enter the second number:");
+int big = getBig(x,y);
+System.out.println("Big 
+}
+
+public static int getBig(int num1,int num2){
+if(num1>num2){
+
+}
+}

1ba2ed21a268cac1f72e514e2e118e56229e0394
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2024-09-07 17:41:30 +0530

    Added a new folder of core java for eclipse

---
+++ Java/CoreJava/src/ClassB.java
@@ -0,0 +1,9 @@
+
+public class ClassB extends ClassA{
+	int x =20;
+	int q=60;
+	
+public int getXofB() {
+	return x;
+}
+}
---
+++ Java/CoreJava/src/DigitBasedProgram/Program1.java
@@ -0,0 +1,11 @@
+package DigitBasedProgram;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}

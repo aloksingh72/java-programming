d1d801ff5cf51db3b360c90c5c8bd83c200e287a
Alok Singh <aloksingh36757@gmail.com>
2024-10-07 13:39:18 +0530

    syntax

--- ArraysPrograms/EvenElementsArray.java
+++ ArraysPrograms/EvenElementsArray.java
@@ -1,22 +1,18 @@
-class EvenElementsArray
-{
-		public static void main(String[] args)
-		{
-			int[] arr = {12,23,45,56,23,8};
-	
-			getEvenElements(arr);
-		}
-		public static void getEvenElements(int[] arr)
-		{
-		
-				for(int i=0;i<arr.length;i++)
-				{
+class EvenElementsArray {
+	public static void main(String[] args) {
+		int[] arr = { 12, 23, 45, 56, 23, 8 };
 
-				if(arr[i] % 2 ==0)
-					{
-						System.out.print(arr[i]+" ");
-					}
-				}
+		getEvenElements(arr);
+	}
+
+	public static void getEvenElements(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] % 2 == 0) {
+				System.out.print(arr[i] + " ");
+			}
 		}
+	}
 
 }
